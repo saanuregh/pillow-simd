e9b04687140147c796769f2c457d4ae84bb3b4ff
Alexander <homm86@gmail.com>
2018-03-30 19:25:40 +0300

    remove sse4 implementation and overlap
    Lut 2560×1600 RGB image
        5³ table to 3D      0.02643 s   154.98 Mpx/s
        17³ table to 3D     0.04478 s    91.47 Mpx/s
        5³ table to 4D      0.04361 s    93.92 Mpx/s
        17³ table to 4D     0.05075 s    80.72 Mpx/s
    
    Load 2560×1600 RGB image
        Jpeg load           0.02833 s   144.57 Mpx/s
        Jpeg save           0.02534 s   161.64 Mpx/s
    
    Lut 2560×1600 RGB image
        5³ table to 3D      0.04390 s    93.30 Mpx/s
        17³ table to 3D     0.04417 s    92.72 Mpx/s
        5³ table to 4D      0.04365 s    93.85 Mpx/s
        17³ table to 4D     0.04494 s    91.14 Mpx/s
    
    Load 2560×1600 RGB image
        Jpeg load           0.02707 s   151.33 Mpx/s
        Jpeg save           0.02514 s   162.94 Mpx/s
    
    Lut 2560×1600 RGB image
        5³ table to 3D      0.02641 s   155.07 Mpx/s
        17³ table to 3D     0.02742 s   149.40 Mpx/s
        5³ table to 4D      0.02767 s   148.05 Mpx/s
        17³ table to 4D     0.02834 s   144.51 Mpx/s

--- src/libImaging/ColorLUT.c
+++ src/libImaging/ColorLUT.c
@@ -55,14 +55,6 @@ ImagingColorLUT3D_linear(Imaging imOut, Imaging imIn, int table_channels,
           With this compensation we never hit the upper cells
           but this also doesn't introduce any noticeable difference. */
     int y, size1D_2D = size1D * size2D;
-    __m128i scale = _mm_set_epi32(0,
-        (size3D - 1) / 255.0 * (1<<SCALE_BITS),
-        (size2D - 1) / 255.0 * (1<<SCALE_BITS),
-        (size1D - 1) / 255.0 * (1<<SCALE_BITS));
-    __m128i scale_mask = _mm_set1_epi32(SCALE_MASK);
-    __m128i index_mul = _mm_set_epi32(
-        0, size1D_2D*table_channels, size1D*table_channels, table_channels);
-    __m128i shuffle3 = _mm_set_epi8(-1,-1, -1,-1, 11,10, 5,4, 9,8, 3,2, 7,6, 1,0);
 #if defined(__AVX2__)
     __m256i scale256 = _mm256_set_epi32(
         0,
@@ -115,7 +107,7 @@ ImagingColorLUT3D_linear(Imaging imOut, Imaging imIn, int table_channels,
         int idx1 = _mm_cvtsi128_si32(_mm256_castsi256_si128(idxs));
         int idx2 = _mm256_extract_epi32(idxs, 4);
 
-        for (; x < imOut->xsize - 1; x += 2) {
+        for (; x < imOut->xsize - 4; x += 2) {
             __m256i next_index = _mm256_mullo_epi32(scale256,
                 _mm256_cvtepu8_epi32(*(__m128i *) &rowIn[x + 2]));
             __m256i next_idxs = _mm256_hadd_epi32(_mm256_hadd_epi32(
@@ -198,77 +190,6 @@ ImagingColorLUT3D_linear(Imaging imOut, Imaging imIn, int table_channels,
     }
     #endif
 
-        __m128i index = _mm_mullo_epi32(scale,
-            _mm_cvtepu8_epi32(*(__m128i *) &rowIn[x]));
-        int idx = _mm_cvtsi128_si32(
-            _mm_hadd_epi32(_mm_hadd_epi32(
-                _mm_madd_epi16(index_mul, _mm_srli_epi32(index, SCALE_BITS)),
-                _mm_setzero_si128()), _mm_setzero_si128()));
-
-        for (; x < imOut->xsize; x++) {
-            __m128i next_index = _mm_mullo_epi32(scale,
-                _mm_cvtepu8_epi32(*(__m128i *) &rowIn[x + 1]));
-            int next_idx = _mm_cvtsi128_si32(
-                _mm_hadd_epi32(_mm_hadd_epi32(
-                    _mm_madd_epi16(index_mul, _mm_srli_epi32(next_index, SCALE_BITS)),
-                    _mm_setzero_si128()), _mm_setzero_si128()));
-
-            __m128i shift = _mm_srli_epi32(
-                _mm_and_si128(scale_mask, index), (SCALE_BITS - SHIFT_BITS));
-
-            __m128i shift1D, shift2D, shift3D;
-            __m128i source, left, right, result;
-            __m128i leftleft, leftright, rightleft, rightright;
-
-            shift = _mm_or_si128(
-                _mm_sub_epi32(_mm_set1_epi32((1<<SHIFT_BITS)-1), shift),
-                _mm_slli_epi32(shift, 16));
-
-            shift1D = _mm_shuffle_epi32(shift, 0x00);
-            shift2D = _mm_shuffle_epi32(shift, 0x55);
-            shift3D = _mm_shuffle_epi32(shift, 0xaa);
-
-            source = _mm_shuffle_epi8(
-                _mm_loadu_si128((__m128i *) &table[idx + 0]), shuffle3);
-            leftleft = _mm_srai_epi32(_mm_madd_epi16(
-                source, shift1D), SHIFT_BITS);
-
-            source = _mm_shuffle_epi8(
-                _mm_loadu_si128((__m128i *) &table[idx + size1D*3]), shuffle3);
-            leftright = _mm_slli_epi32(_mm_madd_epi16(
-                source, shift1D), 16 - SHIFT_BITS);
-
-            source = _mm_shuffle_epi8(
-                _mm_loadu_si128((__m128i *) &table[idx + size1D_2D*3]), shuffle3);
-            rightleft = _mm_srai_epi32(_mm_madd_epi16(
-                source, shift1D), SHIFT_BITS);
-            
-            source = _mm_shuffle_epi8(
-                _mm_loadu_si128((__m128i *) &table[idx + size1D_2D*3 + size1D*3]), shuffle3);
-            rightright = _mm_slli_epi32(_mm_madd_epi16(
-                source, shift1D), 16 - SHIFT_BITS);
-
-            left = _mm_srai_epi32(_mm_madd_epi16(
-                _mm_blend_epi16(leftleft, leftright, 0xaa), shift2D),
-                SHIFT_BITS);
-
-            right = _mm_slli_epi32(_mm_madd_epi16(
-                _mm_blend_epi16(rightleft, rightright, 0xaa), shift2D),
-                16 - SHIFT_BITS);
-
-            result = _mm_madd_epi16(
-                _mm_blend_epi16(left, right, 0xaa), shift3D);
-
-            result = _mm_srai_epi32(_mm_add_epi32(
-                _mm_set1_epi32(PRECISION_ROUNDING<<SHIFT_BITS), result),
-                PRECISION_BITS + SHIFT_BITS);
-
-            result = _mm_packs_epi32(result, result);
-            rowOut[x] = _mm_cvtsi128_si32(_mm_packus_epi16(result, result));
-
-            index = next_index;
-            idx = next_idx;
-        }
     }
 
     return imOut;
